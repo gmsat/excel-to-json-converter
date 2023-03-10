1828107f89b3383f04d1055e0680c989bf3e1fac
mantasge <mantasge@gmail.com>
2023-03-10 11:34:23 +0200

    config for gh-pages deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/excel-to-json-converter/client/",
   plugins: [react()],
   server: {
     port: 3000
